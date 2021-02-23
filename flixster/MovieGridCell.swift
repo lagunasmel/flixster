b6326fb489f649ddfa9b92c30eef06dd0960b2e5
Melissa Lagunas <lagunasmel@gmail.com>
2021-02-22 20:01:05 -0500

    Collection view setup

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Melissa Lagunas on 2/22/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
